a97ccab5b095b50ebb19a38dd793222f99ea07a4
duclairdeugoue <87701146+duclairdeugoue@users.noreply.github.com>
2023-01-21 16:30:50 +0100

    [Component]:: Room: adding the room funcionality

--- app/Views/rooms.php
+++ app/Views/rooms.php
@@ -90,7 +90,9 @@
 <section class="section">
   <div class="container">
 
-    <div class="row">
+    view_cell();
+    
+    <!-- <div class="row">
       <div class="col-md-6 col-lg-4 mb-5" data-aos="fade-up">
         <a href="#" class="room">
           <figure class="img-wrap">
@@ -101,9 +103,45 @@
             <span class="text-uppercase letter-spacing-1">90$ / per night</span>
           </div>
         </a>
-      </div>
+      </div> -->
 
-      <div class="col-md-6 col-lg-4 mb-5" data-aos="fade-up">
+      <!-- <div class="col-md-6 col-lg-4 mb-5" data-aos="fade-up">
+        <a href="#" class="room">
+          <figure class="img-wrap">
+            <img src="images/img_2.jpg" alt="Free website template" class="img-fluid mb-3">
+          </figure>
+          <div class="p-3 text-center room-info">
+            <h2>Family Room</h2>
+            <span class="text-uppercase letter-spacing-1">120$ / per night</span>
+          </div>
+        </a>
+      </div> -->
+
+      <!-- <div class="col-md-6 col-lg-4 mb-5" data-aos="fade-up">
+        <a href="#" class="room">
+          <figure class="img-wrap">
+            <img src="images/img_3.jpg" alt="Free website template" class="img-fluid mb-3">
+          </figure>
+          <div class="p-3 text-center room-info">
+            <h2>Presidential Room</h2>
+            <span class="text-uppercase letter-spacing-1">250$ / per night</span>
+          </div>
+        </a>
+      </div> -->
+
+      <!-- <div class="col-md-6 col-lg-4 mb-5" data-aos="fade-up">
+        <a href="#" class="room">
+          <figure class="img-wrap">
+            <img src="images/img_1.jpg" alt="Free website template" class="img-fluid mb-3">
+          </figure>
+          <div class="p-3 text-center room-info">
+            <h2>Single Room</h2>
+            <span class="text-uppercase letter-spacing-1">90$ / per night</span>
+          </div>
+        </a>
+      </div> -->
+
+      <!-- <div class="col-md-6 col-lg-4 mb-5" data-aos="fade-up">
         <a href="#" class="room">
           <figure class="img-wrap">
             <img src="images/img_2.jpg" alt="Free website template" class="img-fluid mb-3">
@@ -125,43 +163,7 @@
             <span class="text-uppercase letter-spacing-1">250$ / per night</span>
           </div>
         </a>
-      </div>
-
-      <div class="col-md-6 col-lg-4 mb-5" data-aos="fade-up">
-        <a href="#" class="room">
-          <figure class="img-wrap">
-            <img src="images/img_1.jpg" alt="Free website template" class="img-fluid mb-3">
-          </figure>
-          <div class="p-3 text-center room-info">
-            <h2>Single Room</h2>
-            <span class="text-uppercase letter-spacing-1">90$ / per night</span>
-          </div>
-        </a>
-      </div>
-
-      <div class="col-md-6 col-lg-4 mb-5" data-aos="fade-up">
-        <a href="#" class="room">
-          <figure class="img-wrap">
-            <img src="images/img_2.jpg" alt="Free website template" class="img-fluid mb-3">
-          </figure>
-          <div class="p-3 text-center room-info">
-            <h2>Family Room</h2>
-            <span class="text-uppercase letter-spacing-1">120$ / per night</span>
-          </div>
-        </a>
-      </div>
-
-      <div class="col-md-6 col-lg-4 mb-5" data-aos="fade-up">
-        <a href="#" class="room">
-          <figure class="img-wrap">
-            <img src="images/img_3.jpg" alt="Free website template" class="img-fluid mb-3">
-          </figure>
-          <div class="p-3 text-center room-info">
-            <h2>Presidential Room</h2>
-            <span class="text-uppercase letter-spacing-1">250$ / per night</span>
-          </div>
-        </a>
-      </div>
+      </div> -->
 
     </div>
   </div>
