f9c3af815620e0f1ceba6768dbe64562fcfc892c
duclairdeugoue <87701146+duclairdeugoue@users.noreply.github.com>
2023-02-17 23:25:54 +0100

    fixing brand link to index routes

--- app/Views/components/header.php
+++ app/Views/components/header.php
@@ -1,7 +1,7 @@
 <header class="site-header js-site-header">
   <div class="container-fluid">
     <div class="row align-items-center">
-      <div class="col-6 col-lg-4 site-logo" data-aos="fade"><a href="index.html">Sogo Hotel</a></div>
+      <div class="col-6 col-lg-4 site-logo" data-aos="fade"><a href="<?= base_url("/")?>">Sogo Hotel</a></div>
       <div class="col-6 col-lg-8">
 
 
